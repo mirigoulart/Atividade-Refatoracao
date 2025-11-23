8784f7512202b0f0f60c00ff40231fd6012d9466
mirigoulart <mirigoulartdossantos@gmail.com>
2025-11-22 22:40:21 -0300

    Commit 3

--- Customer.java
+++ Customer.java
@@ -26,7 +26,6 @@ public class Customer {
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
 
-
             double thisAmount = amountFor(each);
 
             // add frequent renter points
@@ -50,25 +49,24 @@ public class Customer {
         return result;
     }
 
-
-    private double amountFor(Rental each) {
+    private double amountFor(Rental aRental) {
         double thisAmount = 0;
 
-        switch (each.getMovie().getPriceCode()) {
+        switch (aRental.getMovie().getPriceCode()) {
             case Movie.REGULAR:
                 thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                if (aRental.getDaysRented() > 2)
+                    thisAmount += (aRental.getDaysRented() - 2) * 1.5;
                 break;
 
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                thisAmount += aRental.getDaysRented() * 3;
                 break;
 
             case Movie.CHILDRENS:
                 thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                if (aRental.getDaysRented() > 3)
+                    thisAmount += (aRental.getDaysRented() - 3) * 1.5;
                 break;
         }
 
