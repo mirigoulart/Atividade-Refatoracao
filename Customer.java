d25c58ca696bc27b207c8241928b0ea8018cbfde
mirigoulart <mirigoulartdossantos@gmail.com>
2025-11-22 22:44:19 -0300

    Commit 4

--- Customer.java
+++ Customer.java
@@ -26,50 +26,25 @@ public class Customer {
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
 
-            double thisAmount = amountFor(each);
+         
+            double thisAmount = each.getCharge();
 
             // add frequent renter points
             frequentRenterPoints++;
-            // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
                 each.getDaysRented() > 1) {
                 frequentRenterPoints++;
             }
 
-            // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
-                    String.valueOf(thisAmount) + "\n";
+                      String.valueOf(thisAmount) + "\n";
+
             totalAmount += thisAmount;
         }
 
-        // add footer lines
-        result +=  "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) +
-                " frequent renter points";
+                  " frequent renter points";
         return result;
-    }
-
-    private double amountFor(Rental aRental) {
-        double thisAmount = 0;
-
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (aRental.getDaysRented() > 2)
-                    thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-
-            case Movie.NEW_RELEASE:
-                thisAmount += aRental.getDaysRented() * 3;
-                break;
-
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (aRental.getDaysRented() > 3)
-                    thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-
-        return thisAmount;
     }
 }
